bdf4f805d628ffb6850979f38ad285b6ac01530e
AnhHoangQuach <hoanganh36.work@gmail.com>
2020-07-20 09:00:33 +0700

    connect database and template

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welcome to my website</h1>
+    <div id="wrapper">
+        <div class="chat_wrapper">
+            <div id="chat">
+                <form action="POST">
+                    <textarea name="message" id="" cols="30" rows="10" class="textarea"></textarea>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
